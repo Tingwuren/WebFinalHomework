aae8e1cb0904aecf54ebdb120e821226284e1629
TingNobody <2237617390@qq.com>
2023-12-29 11:33:35 +0800

    Initial commit

---
+++ src/test/java/cn/edu/bupt/shopeasy/ShopEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.bupt.shopeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
